563ef47bb2f5cbb7318c082968c491ea565cbf6d
khady2204 <khady.kandji3@unchk.edu.sn>
2025-01-25 20:57:55 +0000

    Authentification

--- Front-End/src/app/app.component.ts
+++ Front-End/src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterOutlet } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { FormComponent } from './form/form.component';
+import { ConnectionComponent } from './connection/connection.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HomeComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- Front-End/src/app/app.routes.ts
+++ Front-End/src/app/app.routes.ts
@@ -1,3 +1,9 @@
+import { Component } from '@angular/core';
 import { Routes } from '@angular/router';
+import { FormComponent } from './form/form.component';
+import { ConnectionComponent } from './connection/connection.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:'form', component:FormComponent},
+    {path:'connection', component:ConnectionComponent},
+];

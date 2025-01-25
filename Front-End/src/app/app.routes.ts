ffb09ce760881a89af20e121bed32f82de57b3c0
Amadou Boubacar Niang <amadouniang2001@gmail.com>
2025-01-25 21:25:09 +0000

    Authentification

--- Front-End/src/app/app.routes.ts
+++ Front-End/src/app/app.routes.ts
@@ -2,8 +2,11 @@ import { Component } from '@angular/core';
 import { Routes } from '@angular/router';
 import { FormComponent } from './form/form.component';
 import { ConnectionComponent } from './connection/connection.component';
+import { HomeComponent } from './home/home.component';
 
 export const routes: Routes = [
     {path:'form', component:FormComponent},
     {path:'connection', component:ConnectionComponent},
+    {path:'home', component:HomeComponent},
+    { path: '', redirectTo: '/home', pathMatch: 'full' },
 ];

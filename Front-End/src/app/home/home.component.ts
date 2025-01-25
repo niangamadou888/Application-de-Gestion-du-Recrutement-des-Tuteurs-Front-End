d91fc9a1cfc1af5a26309aecd1963f3449d7d06e
Amadou Boubacar Niang <amadouniang2001@gmail.com>
2025-01-25 21:20:11 +0000

    Authentification

--- Front-End/src/app/home/home.component.ts
+++ Front-End/src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
@@ -8,5 +9,18 @@ import { Component } from '@angular/core';
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
+  constructor(private router: Router) {}
 
+  chooseInscription() {
+    
+    // Rediriger vers la page d'inscription
+    this.router.navigate(['/form']);
+  }
+
+
+  chooseConnection() {
+    
+    // Rediriger vers la page de connexion
+    this.router.navigate(['/connection']);
+  }
 }

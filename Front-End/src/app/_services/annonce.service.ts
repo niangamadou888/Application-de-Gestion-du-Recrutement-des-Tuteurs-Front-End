4f8271a20936c123a836ceb1dece12a556c1a3a5
khady2204 <khady.kandji3@unchk.edu.sn>
2025-02-05 18:02:20 +0000

    Annonce

--- Front-End/src/app/_services/annonce.service.ts
+++ Front-End/src/app/_services/annonce.service.ts
@@ -1,9 +1,20 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AnnonceService {
 
-  constructor() { }
+  private apiURL =  "https://application-de-gestion-du-recrutement.onrender.com";
+
+  //injecte HttpClient pour pouvoir effectuer des requêtes HTTP
+  constructor (private http: HttpClient){ }
+
+  // Méthode pour Récupére la liste des annonces depuis l'API
+  getAnnonces(): Observable<any[]> {
+    return this.http.get<any[]>('${this.apiURL}/api/annonces');
+  }
+
 }
--- Front-End/src/app/user-dashbord/user-dashbord.component.ts
+++ Front-End/src/app/user-dashbord/user-dashbord.component.ts
@@ -1,18 +1,30 @@
 import { Component } from '@angular/core';
 import { Route, Router } from '@angular/router';
+import { AnnonceService } from '../_services/annonce.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-user-dashbord',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './user-dashbord.component.html',
   styleUrl: './user-dashbord.component.css'
 })
 export class UserDashbordComponent {
+  annonces: any[] = [];
+
   constructor(
-    private router:Router
-  ){}
+    private router:Router,
+    private annonceService : AnnonceService ){}
   
+  
+  ngOnInit(): void {
+    // Charger les annonces au démarrage du composant
+    this.annonceService.getAnnonces().subscribe((data) => {
+      this.annonces = data;
+      });
+    }
+
    choosePostuler(){
     this.router.navigate(['/form-candidature']);
   }

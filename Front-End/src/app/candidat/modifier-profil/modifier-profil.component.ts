db07a2f5b636a277ed14328c8bc7aa484917e8cb
Pape <pape.sow1@uvs.edu.sn>
2025-02-26 09:24:35 +0000

    modification sur le user-dashbord

---
+++ Front-End/src/app/candidat/modifier-profil/modifier-profil.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { RouterLink, RouterLinkActive } from '@angular/router';
+
+@Component({
+  selector: 'app-modifier-profil',
+  standalone: true,
+  imports: [RouterLink, RouterLinkActive],
+  templateUrl: './modifier-profil.component.html',
+  styleUrl: './modifier-profil.component.css'
+})
+export class ModifierProfilComponent {
+
+}

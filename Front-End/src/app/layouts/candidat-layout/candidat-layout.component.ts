0caeab1434e7579690384ad7d25274685758c84d
Pape <pape.sow1@uvs.edu.sn>
2025-02-27 14:57:06 +0000

    ajustement

--- Front-End/src/app/layouts/candidat-layout/candidat-layout.component.ts
+++ Front-End/src/app/layouts/candidat-layout/candidat-layout.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { FooterComponent } from "../../shared/footer/footer.component";
 import { RouterOutlet } from '@angular/router';
 import { CommonModule } from '@angular/common';
+import { HeaderComponent } from '../../shared/header/header.component';
 
 @Component({
   selector: 'app-candidat-layout',

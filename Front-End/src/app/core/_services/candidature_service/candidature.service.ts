fe71c54b1d28bedaeeec742fbaac6949e8b3739e
khady2204 <khady.kandji3@unchk.edu.sn>
2025-03-04 01:03:40 +0000

    Candidature

--- Front-End/src/app/core/_services/candidature_service/candidature.service.ts
+++ Front-End/src/app/core/_services/candidature_service/candidature.service.ts
@@ -1,21 +1,23 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { UserAuthService } from '../user-auth.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CandidatureService {
 
-  PATH_OF_API = "https://application-de-gestion-du-recrutement.onrender.com/api/candidatures";
+  PATH_OF_API = "https://application-de-gestion-du-recrutement.onrender.com";
 
   constructor(
-    private http: HttpClient
+    private http: HttpClient,
+    private userAuthService : UserAuthService
   ) { }
 
 // Récupérer toutes les candidatures d'un candidat donné
 getCandidatures(userId: string): Observable<any> {
-  return this.http.get(`${this.PATH_OF_API}/candidatures/candidat/${userId}`);
+  return this.http.get<any[]>(`${this.PATH_OF_API}/api/candidatures/${userId}`);
 }
 
 }
